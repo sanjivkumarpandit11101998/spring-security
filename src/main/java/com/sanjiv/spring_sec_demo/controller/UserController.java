383342597fc3f496cc650c4d4041e1b6a5e6b517
sanjiv <sanjivkumarpandit11101998@gmail.com>
2024-07-30 00:04:06 +0530

    security projct

---
+++ src/main/java/com/sanjiv/spring_sec_demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.sanjiv.spring_sec_demo.controller;
+
+import com.sanjiv.spring_sec_demo.model.User;
+import com.sanjiv.spring_sec_demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("register")
+    public User register(@RequestBody User user){
+        return service.saveUser(user);
+    }
+}
---
+++ src/main/java/com/sanjiv/spring_sec_demo/service/UserService.java
@@ -0,0 +1,26 @@
+package com.sanjiv.spring_sec_demo.service;
+
+
+import com.sanjiv.spring_sec_demo.dao.UserRepo;
+import com.sanjiv.spring_sec_demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+    private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
+
+    public User saveUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+
+//    public User saveUser(User user){
+//        user.setPassword
+//    }
+
+}
